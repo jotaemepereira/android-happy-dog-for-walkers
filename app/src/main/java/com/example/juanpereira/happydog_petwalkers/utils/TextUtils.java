23b106c234b45e05c51473dbf0e1d9921a4e7e13
Juan Pereira <juanmanuel.pereira1@gmail.com>
2018-05-18 16:59:45 -0300

    Add unit tests to LoginPresenter

--- app/src/main/java/com/example/juanpereira/happydog_petwalkers/utils/TextUtils.java
+++ app/src/main/java/com/example/juanpereira/happydog_petwalkers/utils/TextUtils.java
@@ -1,10 +1,14 @@
 package com.example.juanpereira.happydog_petwalkers.utils;
 
-import android.util.Patterns;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TextUtils {
 
     public static boolean isValidEmail(String email) {
-        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
